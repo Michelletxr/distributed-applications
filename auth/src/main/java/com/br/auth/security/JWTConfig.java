ac6ef383958d40046dab0f1cc1fc5ad387858c4f
michelle <michelleteixeieramartins@gmail.com>
2023-05-01 17:07:11 -0300

    gerando token de acesso

--- auth/src/main/java/com/br/auth/security/JWTConfig.java
+++ auth/src/main/java/com/br/auth/security/JWTConfig.java
@@ -1,10 +1,9 @@
 package com.br.auth.security;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.br.auth.Service.AuthService;
 import com.br.auth.model.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +16,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
